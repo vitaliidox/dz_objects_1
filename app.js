68078fe738668d99ef6d644835cf8b24319492ef
vitaliidox <72409488+vitaliidox@users.noreply.github.com>
2021-04-16 23:25:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+                        //TASK 1
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+
+// let sum = 0;
+//   for(let key in salaries){
+//     sum += parseInt(salaries[key]);
+//   }
+//     console.log(sum);
+
+                        //Task 2
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   };
+
+//   function multiplyNumeric(menu){
+//         for(let key in menu){
+//         if (typeof menu[key] == 'number'){
+//             menu[key]*2
+//         }
+//         }
+//       console.log(menu)
+//   }
+//   multiplyNumeric(menu)
+
+                        //Task 3
+// let menu = {
+//     width: 400,
+//     height: 600,
+//     title: "My menu"
+//   };
+
+//   function isEmpty(menu){
+//       for(let key in menu){
+//           return false
+//       }
+//       return true
+//   }
+
+//  console.log(isEmpty(menu)) 
